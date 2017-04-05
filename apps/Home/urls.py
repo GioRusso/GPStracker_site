2f94fcebf07308d8b5a77ad442fcd75e377af4ed
sorelyss <sorboy11@gmail.com>
2017-04-05 02:33:20 -0500

    Add files via upload

--- apps/Home/urls.py
+++ apps/Home/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     url(r'^fecha', views.Fechas, name = 'fechas'),
     url(r'^real_time', views.TiempoReal, name = 'real_time'),
     url(r'^update', views.Update, name = 'update'),
+
+    url(r'^historial_cel$', views.Historial_Cel, name = 'historial_cel'),
+    url(r'^fec_cel', views.Fechas_Cel, name = 'fechas_cel'),
+
     url(r'^historial_area$', views.Historial_by_area, name = 'historial_by_area'),
     url(r'^areaquery', views.Area, name = 'area'),
     url(r'^historial_And_Area$', views.Historial_Area, name = 'historial_area'),
-    url(r'^fecha_area', views.Fechas_Area, name = 'fechas_area'),
+    url(r'^dat_area', views.Fechas_Area, name = 'fechas_area'),
 ]
