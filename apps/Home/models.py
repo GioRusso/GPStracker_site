8df44699872e492416ea53464905e4b8bbe39ba1
Sorelys Sandoval <sorboy11@gmail.com>
2017-05-16 20:58:29 -0500

    Update models.py

--- apps/Home/models.py
+++ apps/Home/models.py
@@ -2,6 +2,8 @@
 from django.db import models
 
 class Data(models.Model):
+	device = models.CharField(max_length=30, default='syrus')
 	longitude = models.CharField(max_length=30)
 	latitude = models.CharField(max_length=30)
+	elevation = models.CharField(max_length=30)
 	date = models.DateTimeField(max_length=30)
